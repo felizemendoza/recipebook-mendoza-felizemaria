56ed7c92a8c5c48c9562dab4cd789eb2fe36f26b
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-02-09 15:57:57 +0800

    Set up Django

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&&2&27s+&z1hsgja8qk%wb0nf%fk2#759pk()vuxee)e5teq8f'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
