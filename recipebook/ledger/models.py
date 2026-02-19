45065efd0262059c6bed7311ec2c727bd9601c34
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-02-19 13:41:31 +0800

    View uses Recipe model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -15,7 +15,10 @@ class Ingredient(models.Model):
         verbose_name_plural = 'ingredients'
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
 
     def get_absolute_url(self):
         return reverse('ledger:recipe_detail', args=[str(self.id)])
@@ -29,12 +32,12 @@ class RecipeIngredient(models.Model):
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
-        related_name='ingredient'
+        related_name='recipe'
         )
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        related_name='recipe'
+        related_name='ingredients'
         )
     
-    
+
