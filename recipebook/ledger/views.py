9b75d64f39eb98932c128639dbcf6f7de3da8718
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-02-19 08:41:38 +0800

    Fixed project-level url

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -28,7 +28,7 @@ def recipes(request):
                     "quantity": "1 packet"
                 }
             ],
-            "link": "/ledger/recipe/1"
+            "link": "/recipe/1"
         },
         {
             "name": "Recipe 2",
@@ -62,7 +62,7 @@ def recipes(request):
                     "quantity": "1 kilo"
                 }
             ],
-            "link": "/ledger/recipe/2"
+            "link": "/recipe/2"
             }
         ]
     }
