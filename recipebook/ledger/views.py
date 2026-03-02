92b7b87ef5a65cab13d5adbc74a894167cc33933
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-03-02 17:39:37 +0800

    Created custom login flow and password reset flow

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from .models import Recipe
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class RecipeListView(ListView):
     model = Recipe
     template_name = "recipe_list.html"
 
 
-class RecipeDetailView(DetailView):
+class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "recipe.html"
+    redirect_field_name = "registration/login"
