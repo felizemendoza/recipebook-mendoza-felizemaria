01fad4a77c3b113e46f6963c716918d1bba212bd
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-03-02 08:13:41 +0800

    Created Profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.TextField(max_length=255)
 
 
 class Ingredient(models.Model):
