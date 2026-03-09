5040ab9f3849ac2b8f3b53cc86510d6f52696a31
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-03-09 22:10:11 +0800

    Changed Profile's related name

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -32,7 +32,7 @@ class Ingredient(models.Model):
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     author = models.ForeignKey(
-        Profile, on_delete=models.CASCADE, related_name="author", null=True
+        Profile, on_delete=models.CASCADE, related_name="recipes", null=True
     )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
