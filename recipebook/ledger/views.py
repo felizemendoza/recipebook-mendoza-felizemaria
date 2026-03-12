d3f3c518765e581714854c864aaa70cc4a9d4c12
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-03-12 18:54:32 +0800

    Created page for users to create recipes

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Recipe
+from .forms import RecipeForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -13,4 +15,15 @@ class RecipeListView(ListView):
 class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = "recipe.html"
+    form_class = RecipeForm
     redirect_field_name = "registration/login"
+
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    template_name = "recipe_add.html"
+    form_class = RecipeForm
+    redirect_field_name = "registration/login"
+
+    # def form_valid(self, form):
+    #     form.instance.created_by = self.request.user
+    #     return super().form_valid(form)
