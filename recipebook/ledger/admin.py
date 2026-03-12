5ac7662e6b7a39b460d197ba2ea3559af61f9c03
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-03-12 14:46:41 +0800

    Modified RecipeAdmin to contain uploaded images and modified recipe.html to display images

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, RecipeIngredient, Profile
+from .models import Recipe, RecipeIngredient, Profile, RecipeImage
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
@@ -18,12 +18,15 @@ class UserAdmin(BaseUserAdmin):
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
 
+class RecipeImageInline(admin.TabularInline):
+    model = RecipeImage
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
     inlines = [
-        RecipeIngredientInline,
+        RecipeIngredientInline, RecipeImageInline
     ]
+
 
 
 admin.site.register(Recipe, RecipeAdmin)
