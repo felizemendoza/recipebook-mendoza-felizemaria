bcc4a3118d08d6df7af26e681d67b210746287e0
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-03-12 14:03:23 +0800

    Created RecipeImage model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -59,3 +59,8 @@ class RecipeIngredient(models.Model):
 
     def __str__(self):
         return f"{self.quantity} {self.ingredient.name}"
+
+class RecipeImage(models.Model):
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField(max_length=255)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="image")
