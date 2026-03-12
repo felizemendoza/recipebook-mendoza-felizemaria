be691d30a0f54df7164a6fed8da3c2fcd52893b6
felizemendoza <felize.maria.mendoza@student.ateneo.edu>
2026-03-12 21:39:23 +0800

    Added the option to add images in recipes

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import RecipeListView, RecipeDetailView, RecipeCreateView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, RecipeImageCreateView
 
 urlpatterns = [
     path("recipes/list/", RecipeListView.as_view(), name="recipe_list"),
     path("recipe/<int:pk>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("recipe/add/", RecipeCreateView.as_view(), name="recipe_create"),
+    path("recipe/<int:pk>/add_image/", RecipeImageCreateView.as_view(), name="add_image"),
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
-from .models import Recipe
-from .forms import RecipeForm
+from django.shortcuts import render, redirect
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
 class RecipeListView(ListView):
@@ -24,6 +25,23 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
     form_class = RecipeForm
     redirect_field_name = "registration/login"
 
-    # def form_valid(self, form):
-    #     form.instance.created_by = self.request.user
-    #     return super().form_valid(form)
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    template_name = "recipe_add_image.html"
+    form_class = RecipeImageForm
+    redirect_field_name = "registration/login"
+
+    def get_success_url(self):
+        return reverse_lazy('ledger:recipe_detail', kwargs={ 'pk': self.kwargs['pk'] })
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recipe'] = RecipeImageForm()
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.instance.recipe_id = self.kwargs['pk']
+            form.save()
+            return redirect(self.get_success_url())
